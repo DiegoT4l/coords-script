00a8d03c7c3567944c2bbf160d21c41c3fea216f
DiegoT4l <lopez.diiego.107@hotmail.com>
2024-11-13 01:27:16 -0600

    Add fxmanifest.lua for project configuration and resource management

---
+++ src/fxmanifest.lua
@@ -0,0 +1,25 @@
+---@diagnostic disable: undefined-global
+
+fx_version 'adamant'
+game 'gta5'
+lua54 'yes'
+
+author 'DiegoT4l'
+description 'An awesome, but short, description'
+version '1.0.0'
+
+server_scripts {
+	'server/server.js'
+}
+
+client_scripts {
+	'client/client.js'
+}
+
+ui_page 'web/index.html'
+
+files {
+	'web/index.html',
+	'web/script.js',
+	'web/styles.css'
+}
